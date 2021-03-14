3dc2df03bc9535ef944a6fd3988019bfd4e054a4
Laozi <72474323+DontReadUserNames@users.noreply.github.com>
2021-03-14 04:54:22 +0200

    First

---
+++ DotNetClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class TcpTimeClient
+{
+    public static async Task Main(string[] args)
+    {
+        var client = new TcpClient();
+
+        try
+        {
+            Console.WriteLine($"Connecting to the server..");
+
+            await client.ConnectAsync("localhost", 6060);
+        }
+        catch
+        {
+            int retryPeriod = 9;
+
+            Console.Write($"Couldn't connect to the server. Trying again in {retryPeriod} second(s)");
+
+            for (var i = retryPeriod; i > 0; i--)
+            {
+                Thread.Sleep(1000);
+
+                Console.Write("\b \b");
+                Console.Write(i);
+            }
+        }
+
+        //NetworkStream ns = client.GetStream();
+
+        //while (true)
+        //{
+        //    if (ns.DataAvailable)
+        //    {
+        //        byte[] bytes = new byte[1024];
+        //        int bytesRead = ns.Read(bytes, 0, bytes.Length);
+
+        //        Console.Write(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+        //    }
+        //}
+    }
+}
---
+++ DotNetServer/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetServer
+{
+    class Program
+    {
+        private static List<TcpClient> ConnectedClients = new List<TcpClient>();
+
+        private static Queue<char> Messages = new Queue<char>();
+
+        private static async void Listen()
+        {
+            listener = new TcpListener(IPAddress.Any, 6060);
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                ConnectedClients.Add(client);
+            }
+        }
+
+        private static TcpListener listener;
+
+        private static bool IsSending { get; set; }
+
+        static async Task Send()
+        {
+            IsSending = true;
+
+            char? message = ConnectedClients.Count > 0 && Messages.Count > 0 ? Messages.Dequeue() : null;
+
+            while (message != null)
+            {
+                foreach (var client in ConnectedClients)
+                {
+                    NetworkStream ns = client.GetStream();
+
+                    byte[] messageToSend = Encoding.Unicode.GetBytes(message.ToString());
+
+                    await ns.WriteAsync(messageToSend, 0, messageToSend.Length);
+                }
+
+                message = ConnectedClients.Count > 0 && Messages.Count > 0 ? Messages.Dequeue() : null;
+            }
+
+            IsSending = false;
+        }
+
+        static async Task Main(string[] args)
+        {
+            await Task.Run(Listen);
+
+            Console.WriteLine("Type your letters..");
+
+            while (true)
+            {
+                var pressedKey = Console.ReadKey(true);
+
+                if (!char.IsControl(pressedKey.KeyChar))
+                {
+                    Console.Write(pressedKey.KeyChar);
+
+                    Messages.Enqueue(pressedKey.KeyChar);
+
+                    if (!IsSending)
+                    {
+                        await Send();
+                    }
+                }
+            }
+        }
+    }
+}
