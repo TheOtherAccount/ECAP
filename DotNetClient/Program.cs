d1837d0e86c87d4aaae1077dd88dece7cc04e397
Laozi <72474323+DontReadUserNames@users.noreply.github.com>
2021-03-14 18:44:12 +0200

    Python server

--- DotNetClient/Program.cs
+++ DotNetClient/Program.cs
@@ -95,3 +95,5 @@ public class DotNetClient
         }
     }
 }
+
+//mozo //todo setcursorposition
--- PythonServer/PythonServer.py
+++ PythonServer/PythonServer.py
@@ -1,1 +1,33 @@
-print('hello world')
+import msvcrt
+import socket
+from collections import deque
+
+portNumber = 6060
+
+connectedClients = []
+messageQueue = deque()
+
+theStocket = socket.socket()
+theStocket.bind(('', portNumber))
+theStocket.listen()
+
+print('Please start typing.. and hit ESC when you want to exit.')
+
+#while True:
+#	conn, addr = theStocket.accept()
+#	connectedClients.append(conn)
+
+#conn.sendall('5'.encode("utf-16-le"))
+
+
+
+while True:
+	theKey = msvcrt.getch()
+	keyNumber = ord(theKey)
+	if(keyNumber == 27):
+		break
+	elif(keyNumber > 0 and keyNumber < 128 and keyNumber != 13):
+		theChar = chr(keyNumber)
+		messageQueue.append(theChar)
+		print(theChar, end='')
+		sendMessages()
