5baa42c57feda96a8e1a6c6f72c26cfb62a66408
Laozi <72474323+DontReadUserNames@users.noreply.github.com>
2021-03-17 15:33:44 +0200

    Commenting message event args class

--- DotNetClient/MessageEventArgs.cs
+++ DotNetClient/MessageEventArgs.cs
@@ -1,14 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+/// <summary>
+/// Holding the arguemtns of the the MessageReceived event.
+/// </summary>
 public class MessageEventArgs : EventArgs
 {
+    /// <summary>
+    /// The message that has been received.
+    /// </summary>
+    public string Message { get; set; }
+    /// <summary>
+    /// Instaniates an object with the specified message.
+    /// </summary>
+    /// <param name="message"></param>
     public MessageEventArgs(string message)
     {
         Message = message;
     }
-    public string Message { get; set; }
 }
