2a8db92dcb3dc8ddb14c3b236b066c97d1d51788
Laozi <72474323+DontReadUserNames@users.noreply.github.com>
2021-03-17 16:50:56 +0200

    Commeting eCAPClient

--- DotNetClient/ECAPClient.cs
+++ DotNetClient/ECAPClient.cs
@@ -3,30 +3,63 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Connects to a remote server via TCP/IP and receives messages.
+/// </summary>
 public class ECAPClient
 {
-    public ConnectionInfo ConnectionInfo { get; set; }
-
+    /// <summary>
+    /// The main tcpClient object used for the communication.
+    /// </summary>
     private static TcpClient tcpClient;
-
+    /// <summary>
+    /// An object holding the connection information of the server that you want to connect to.
+    /// </summary>
+    public ConnectionInfo ConnectionInfo { get; set; }
+    /// <summary>
+    /// Message event handler used mainly for the MessageReceived event.
+    /// </summary>
+    /// <param name="sender">The publisher of the event, typically the ecpClient object.</param>
+    /// <param name="e">An EventArgs object contains the message that has been received.</param>
     public delegate void MessageEventHandler(object sender, MessageEventArgs e);
-
+    /// <summary>
+    /// An event that gets raised while connecting to the server.
+    /// </summary>
     public event EventHandler Connecting;
+    /// <summary>
+    /// An event that gets raised when the client is unable to connect to the server.
+    /// </summary>
     public event EventHandler ConnectionRefused;
+    /// <summary>
+    /// An event that gets raised when the connection is established with the server.
+    /// </summary>
     public event EventHandler ConnectionEstablished;
+    /// <summary>
+    /// An event that gets raised when a message has been received.
+    /// </summary>
     public event MessageEventHandler MessageReceived;
+    /// <summary>
+    /// An event that gets raised when an open connection gets lost.
+    /// </summary>
     public event EventHandler ConnectionLost;
-
+    /// <summary>
+    /// Instaniates an object with the specified connectionInfo object.
+    /// </summary>
+    /// <param name="connectionInfo">An object holds the connection information required to connect to the server.</param>
     public ECAPClient(ConnectionInfo connectionInfo)
     {
         ConnectionInfo = connectionInfo;
     }
-
+    /// <summary>
+    /// Starts the connection to the remove host.
+    /// </summary>
     public async Task Start()
     {
         await Connect();
     }
-
+    /// <summary>
+    /// Tries to establish a connection with the server.
+    /// </summary>
     private async Task Connect()
     {
         try
@@ -44,8 +77,10 @@ public class ECAPClient
             await OnConnectionRefused(EventArgs.Empty);
         }
     }
-
-    private async Task WaitMessages()
+    /// <summary>
+    /// Keep receiving messages until the connection is lost.
+    /// </summary>
+    private async Task ReceiveMessages()
     {
         NetworkStream ns = tcpClient.GetStream();
 
@@ -58,7 +93,7 @@ public class ECAPClient
             {
                 receivedByteCount = await ns.ReadAsync(message, 0, message.Length);
             }
-            catch 
+            catch
             {
                 await OnConnectionLost(EventArgs.Empty);
             }
@@ -75,7 +110,9 @@ public class ECAPClient
             }
         }
     }
-
+    /// <summary>
+    /// Closes the underlying tcpClient, raises the appropriate event and starts over the process.
+    /// </summary>
     protected virtual async Task OnConnectionLost(EventArgs args)
     {
         tcpClient.Close();
@@ -87,7 +124,9 @@ public class ECAPClient
 
         await Start();
     }
-
+    /// <summary>
+    /// Raises the event and starts receiving the messages.
+    /// </summary>
     protected virtual async Task OnConnectionEstablished(EventArgs args)
     {
         if (ConnectionEstablished != null)
@@ -95,9 +134,12 @@ public class ECAPClient
             ConnectionEstablished(this, args);
         }
 
-        await WaitMessages();
+        await ReceiveMessages();
     }
-
+    /// <summary>
+    /// It just raises the event.
+    /// </summary>
+    /// <param name="args">An object contains the message that has been received.</param>
     protected virtual void OnMessageReceived(MessageEventArgs args)
     {
         if (MessageReceived != null)
@@ -105,7 +147,9 @@ public class ECAPClient
             MessageReceived(this, args);
         }
     }
-
+    /// <summary>
+    /// It just raises the event.
+    /// </summary>
     protected virtual void OnConnecting(EventArgs args)
     {
         if (Connecting != null)
@@ -113,7 +157,9 @@ public class ECAPClient
             Connecting(this, args);
         }
     }
-
+    /// <summary>
+    /// Raises the event and trying to connect again.
+    /// </summary>
     protected virtual async Task OnConnectionRefused(EventArgs args)
     {
         if (ConnectionRefused != null)
